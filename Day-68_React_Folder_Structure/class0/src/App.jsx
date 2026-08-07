36a8284d6ce565b1d5ff19854e42aa81818ff727
PoojaBayappagol <poojabayappagol531@gmail.com>
2026-08-07 09:30:00 +0530

    initial commit

---
+++ Day-68_React_Folder_Structure/class0/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function abc() {
+  return React.createElement("h1", null, "Hello everyone");
+}
+
+export default abc;
---
+++ Day-68_React_Folder_Structure/class0/src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import './index.css'
+import App from './App.jsx'
+
+var main=ReactDOM.createRoot(document.querySelector('#root'))
+main.render(<App />);
