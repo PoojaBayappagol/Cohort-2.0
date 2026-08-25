e25c5ed657e19c46e55604961b3687394979aae7
PoojaBayappagol <poojabayappagol531@gmail.com>
2026-08-25 21:55:49 +0530

    database

---
+++ Day-90_Database/src/config/database.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongoose')
+const dns=require('node:dns/promises')
+
+dns.setServers(['1.1.1.1','8.8.8.8'])
+
+function connectToDB(){
+    mongoose.connect(process.env.MONGO_URI)
+
+        .then(()=>{
+            console.log("Connected to DB")
+        })
+}
+
+
+module.exports=connectToDB
